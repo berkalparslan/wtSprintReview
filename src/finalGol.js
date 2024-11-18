7b8f93ad5f767740105e87bede88156c37c51602
berkalparslan <aberkalparslan@gmail.com>
2024-11-18 21:00:00 +0300

    ilk commit

---
+++ src/finalGol.js
@@ -0,0 +1,27 @@
+const { fifaData } = require('../data/fifa');
+
+const finallerdekiGolSayilari = (fifaData) => {
+  golSayilari = {};
+  const finalMaclar = fifaData.filter((mac) => mac.Stage === 'Final');
+  finalMaclar.forEach((mac) => {
+    const homeTeam = mac['Home Team Name'];
+    const awayTeam = mac['Away Team Name'];
+    const homeGoals = mac['Home Team Goals'];
+    const awayGoals = mac['Away Team Goals'];
+
+    if (golSayilari[homeTeam] !== undefined) {
+      golSayilari[homeTeam] += homeGoals;
+    } else {
+      golSayilari[homeTeam] = homeGoals;
+    }
+    if (golSayilari[awayTeam] !== undefined) {
+      golSayilari[awayTeam] += awayGoals;
+    } else {
+      golSayilari[awayTeam] = awayGoals;
+    }
+  });
+  return golSayilari;
+};
+
+const result = finallerdekiGolSayilari(fifaData);
+console.log(result);
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+
+console.log(chalk.green("This is a green text!"));
+console.log(chalk.blue("This is a blue text!"));
+console.log(chalk.red("This is a red text!"));
