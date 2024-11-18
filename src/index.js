33f88593889e5394f8ebab8e79eb40bdc38c8dad
berkalparslan <34027499+berkalparslan@users.noreply.github.com>
2024-11-18 21:04:35 +0300

    Update index.js from web

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import chalk from "chalk";
 
 console.log(chalk.green("This is a green text!"));
 console.log(chalk.blue("This is a blue text!"));
-console.log(chalk.red("This is a red text!"));
+
+console.log("edit from web");
